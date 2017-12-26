e09cbebbe16cf6dc6d3db84c0ddfa1373440bb8f
Artashes Amatuni <artashes.amatuni@gmail.com>
2017-12-26 11:43:12 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Artashes Amatuni
+ *
+ * Created on December 26, 2017, 11:38 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+void main(void) {
+
+
+}
+
