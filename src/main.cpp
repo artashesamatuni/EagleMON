15f65bd4c043fa866b2594bd9664af06287e64da
AndranikSanoyan <31041224+AndranikSanoyan@users.noreply.github.com>
2017-12-28 12:15:47 +0400

    HTTP post part

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 #include "../interfaces/mcp3204.h"
 #include "../interfaces/digital_input.h"
 
+//**************  testing web  **********
+#include <unistd.h> /* read, write, close */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
+#include <cstdlib>
+#include <string.h>
+void error(const char *msg) { perror(msg); exit(0); }
+
 using namespace std;
 
 //#include "lasote/httpserver/http_server.h"
@@ -35,6 +44,89 @@ int ADC_Read(int ch)
 
 
 int main(void) {
+
+    int portno =        80;
+    char *host =        "localhost";
+    char *message_fmt = "POST /status/{Tempr:25}";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, received, total;
+    char message[1024],response[4096];
+
+    //if (argc < 3) { puts("Parameters: <apikey> <command>"); exit(0); }
+
+    /* fill in the parameters */
+    sprintf(message,message_fmt);
+    printf("Request:\n%s\n",message);
+
+    /* create the socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+
+    /* lookup the ip address */
+    server = gethostbyname(host);
+    if (server == NULL) error("ERROR, no such host");
+
+    /* fill in the structure */
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    /* connect the socket */
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    /* send the request */
+    total = strlen(message);
+    sent = 0;
+    do {
+        bytes = write(sockfd,message+sent,total-sent);
+        if (bytes < 0)
+            error("ERROR writing message to socket");
+        if (bytes == 0)
+            break;
+        sent+=bytes;
+    } while (sent < total);
+
+    /* receive the response */
+    memset(response,0,sizeof(response));
+    total = sizeof(response)-1;
+    received = 0;
+    do {
+        bytes = read(sockfd,response+received,total-received);
+        if (bytes < 0)
+            error("ERROR reading response from socket");
+        if (bytes == 0)
+            break;
+        received+=bytes;
+    } while (received < total);
+
+    if (received == total)
+        error("ERROR storing complete response from socket");
+
+    /* close the socket */
+    close(sockfd);
+
+    /* process response */
+    printf("Response:\n%s\n",response);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     // ADC testing
     int ch1_data = ADC_Read(0);
@@ -76,12 +168,12 @@ int main(void) {
             if(Relay_1.state())
             {
             Relay_1.off();
-            delay(100);
+            delay(500);
             }
             else
             {
             Relay_1.on();
-            delay(100);
+            delay(500);
             }
         }
     
